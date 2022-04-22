1c563dd8ba1cab03c8040504138cde2567d5e9c4
Eric Wahlquist <wahleric@cs.washington.edu>
2022-04-22 06:21:46 -0700

    Refactor styling of projects page

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,13 +3,13 @@ import '../css/Card.css';
 function Card(props) {
 	return (
 		<div className="card" onClick={props.onClickCallback}>
-			<div className="card-image">
+			<div className="card-field image">
 				<img src={`${process.env.PUBLIC_URL}/${props.image}`} alt={props.title} />
 			</div>
-			<div className="card-title">
+			<div className="card-field title">
 				{props.title}
 			</div>
-			<div className="card-description">
+			<div className="card-field description">
 				{props.description}
 			</div>
 		</div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,10 +8,7 @@ function Projects() {
 
 	return (
 		<div className="projects">
-			<div className="field intro">
-				Below is a collection of my personal developer projects. Click on a project card to see additional details about the project, including the languages and libraries involved
-			</div>
-			<div className="field projectslist">
+			<div className="projectslist">
 				{Object.keys(projects).map((projectId, index) => {
 					let project = projects[projectId] || {};
 					return <Card key={index} title={project.title} image={project.primaryImage} description={project.shortDescription} onClickCallback={() => {navigate(projectId);}}/>

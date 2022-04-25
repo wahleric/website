921ee5102ef43494c3ef9a251eaa3130f7e94791
Eric Wahlquist <wahleric@cs.washington.edu>
2022-04-25 05:40:33 -0700

    Implement card carousel

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,20 +2,34 @@ import '../css/Projects.css';
 import Card from '../components/Card';
 import { useNavigate } from 'react-router-dom';
 
+import CardCarousel from './CardCarousel';
+
+
 function Projects() {
 	let projects = require('../data/projects.json');
 	let navigate = useNavigate();
 
 	return (
-		<div className="projects">
+		<div className="temp">
+			<CardCarousel items={projects} />
+		</div>
+
+
+
+
+
+
+
+	);
+{/*
+			<div className="projects">
 			<div className="projectslist">
 				{Object.keys(projects).map((projectId, index) => {
 					let project = projects[projectId] || {};
 					return <Card key={index} title={project.title} image={project.primaryImage} description={project.shortDescription} onClickCallback={() => {navigate(projectId);}}/>
 				})}
 			</div>
-		</div>
-	);
+		</div>*/}
 }
 
 export default Projects;
